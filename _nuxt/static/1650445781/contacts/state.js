100abcd92955014432c489fa8f45d47c4bf89170
Mansurkhoja <68527235+Mansurkhoja@users.noreply.github.com>
2022-04-20 14:14:45 +0500

    Add files via upload

---
+++ _nuxt/static/1650445781/contacts/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650445781",layout:"default",error:b,state:{preloader:{isFinished:a,isFinishedDelay:a},theme:{isDark:a}},serverRendered:true,routePath:"\u002Fcontacts",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:b}}}}(false,null));
